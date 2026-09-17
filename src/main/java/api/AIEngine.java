4527af1bac630e5dedcec0a83e9a28829ca9401e
Rakesh Kumawath <30873433+rk2216@users.noreply.github.com>
2026-09-17 17:16:46 +0530

    Adding getSmartMove, Player.flip and renaming getCell to getSymbol

--- src/main/java/api/AIEngine.java
+++ src/main/java/api/AIEngine.java
@@ -10,16 +10,73 @@ public class AIEngine {
     public Move suggestMove(Player computer, Board board) {
         if(board instanceof TicTacToeBoard) {
             TicTacToeBoard board1 = (TicTacToeBoard) board;
-            for(int i=0; i<3; i++) {
-                for(int j=0; j<3; j++) {
-                    if(board1.getCell(i, j) == null) {
-                        return new Move(new Cell(i, j), computer);
-                    }
-                }
+            Move suggestion;
+            if(isStarting(board1, 4)) {
+                suggestion = getBasicMove(computer, board1);
+            } else {
+                suggestion = getSmartMove(computer, board1);
             }
+            if(suggestion != null)
+                return suggestion;
             throw new IllegalStateException();
         } else {
             throw new IllegalArgumentException();
         }
+    }
+
+    private boolean isStarting(TicTacToeBoard board, int threshold) {
+        int count = 0;
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                if(board.getSymbol(i, j) != null) {
+                    count++;
+                }
+            }
+        }
+
+        return count < threshold;
+    }
+
+    private Move getBasicMove(Player computer, TicTacToeBoard board) {
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                if(board.getSymbol(i, j) == null) {
+                    return new Move(new Cell(i, j), computer);
+                }
+            }
+        }
+        return null;
+    }
+
+    private Move getSmartMove(Player computer, TicTacToeBoard board) {
+        RuleEngine ruleEngine = new RuleEngine();
+
+        //Attacking Move
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                if(board.getSymbol(i, j) == null) {
+                    Move move = new Move(new Cell(i, j), computer);
+                    board.move(move);
+                    if(ruleEngine.getState(board).isOver()) {
+                        return move;
+                    }
+                }
+            }
+        }
+
+        //Defensive Move
+        for(int i=0; i<3; i++) {
+            for(int j=0; j<3; j++) {
+                if(board.getSymbol(i, j) == null) {
+                    Move move = new Move(new Cell(i, j), computer.flip());
+                    board.move(move);
+                    if(ruleEngine.getState(board).isOver()) {
+                        return new Move(new Cell(i, j), computer);
+                    }
+                }
+            }
+        }
+
+        return getBasicMove(computer, board);
     }
 }
