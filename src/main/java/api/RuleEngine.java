5fa63b745f2a9538971a798faffd6da6d34fff0c
Rakesh Kumawath <30873433+rk2216@users.noreply.github.com>
2026-09-18 16:02:20 +0530

    RuleSet class. Move logic to TicTacToeBoard. Liskov by not adding getSymbol to Board (#21)
    RuleSet class. Move logic to TicTacToeBoard. Liskov by not adding getSymbol to Board.

--- src/main/java/api/RuleEngine.java
+++ src/main/java/api/RuleEngine.java
@@ -3,39 +3,16 @@ package api;
 import boards.TicTacToeBoard;
 import game.*;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 public class RuleEngine {
 
-    Map<String, List<Rule<TicTacToeBoard>>> ruleMap = new HashMap<>();
+    Map<String, RuleSet> ruleMap = new HashMap<>();
 
     public RuleEngine() {
         String key = TicTacToeBoard.class.getName();
-        ruleMap.put(key, new ArrayList<>());
-        ruleMap.get(key).add(new Rule<>(board -> outerTraversal((i, j) -> board.getSymbol(i, j))));
-        ruleMap.get(key).add(new Rule<>(board -> outerTraversal((i, j) -> board.getSymbol(j, i))));
-        ruleMap.get(key).add(new Rule<>(board -> traverse((i) -> board.getSymbol(i, i))));
-        ruleMap.get(key).add(new Rule<>(board -> traverse((i) -> board.getSymbol(i, 2-i))));
-        ruleMap.get(key).add(new Rule<>(board -> {
-            int countOfFilledCells = 0;
-            for(int i=0; i<3; i++) {
-                for(int j=0; j<3; j++) {
-                    if(board.getSymbol(i, j) != null) {
-                        countOfFilledCells++;
-                    }
-                }
-            }
-            if(countOfFilledCells == 9) {
-                return new GameState(true, "-");
-            }
-
-            return new GameState(false, "-");
-        }));
+        ruleMap.put(key, TicTacToeBoard.getRules());
     }
 
     public GameInfo getInfo(Board board) {
@@ -85,7 +62,7 @@ public class RuleEngine {
     public GameState getState(Board board) {
         if(board instanceof TicTacToeBoard) {
             TicTacToeBoard tBoard = (TicTacToeBoard) board;
-            List<Rule<TicTacToeBoard>> rules = ruleMap.get(TicTacToeBoard.class.getName());
+            RuleSet<TicTacToeBoard> rules = ruleMap.get(TicTacToeBoard.class.getName());
             for(Rule<TicTacToeBoard> r : rules) {
                 GameState gameState = r.condition.apply(tBoard);
                 if(gameState.isOver()) {
@@ -96,40 +73,5 @@ public class RuleEngine {
         return new GameState(false, "-");
     }
 
-    private GameState outerTraversal(BiFunction<Integer, Integer, String> next) {
-        GameState result = new GameState(false, "-");
-        for(int i=0; i<3; i++) {
-            final int ii = i;
-            Function<Integer, String> traversal = (j) -> next.apply(ii, j);
-            GameState innerTraversal = traverse(traversal);
-            if(innerTraversal.isOver()) {
-                result = innerTraversal;
-            }
-        }
-        return result;
-    }
-
-    private GameState traverse(Function<Integer, String> traversal) {
-        GameState result = new GameState(false, "-");
-        boolean possibleStreak = true;
-        for(int j = 0; j <3; j++) {
-            if(traversal.apply(j) == null || !traversal.apply(0).equals(traversal.apply(j))) {
-                possibleStreak = false;
-                break;
-            }
-        }
-        if(possibleStreak) {
-            result = new GameState(true, traversal.apply(0));
-        }
-        return result;
-    }
-}
-
-class Rule<T extends Board> {
-    Function<T, GameState> condition;
-
-    public Rule(Function<T, GameState> condition) {
-        this.condition = condition;
-    }
 }
 
--- src/main/java/boards/TicTacToeBoard.java
+++ src/main/java/boards/TicTacToeBoard.java
@@ -1,13 +1,73 @@
 package boards;
 
+import api.Rule;
+import api.RuleSet;
 import game.Board;
 import game.Cell;
+import game.GameState;
 import game.Move;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class TicTacToeBoard implements Board {
     String[][] cells = new String[3][3];
     public String getSymbol(int i, int j) {
         return cells[i][j];
+    }
+
+    public static RuleSet<TicTacToeBoard> getRules() {
+        RuleSet rules = new RuleSet();
+        rules.add(new Rule<TicTacToeBoard>(board -> outerTraversal((i, j) -> board.getSymbol(i, j))));
+        rules.add(new Rule<TicTacToeBoard>(board -> outerTraversal((i, j) -> board.getSymbol(j, i))));
+        rules.add(new Rule<TicTacToeBoard>(board -> traverse((i) -> board.getSymbol(i, i))));
+        rules.add(new Rule<TicTacToeBoard>(board -> traverse((i) -> board.getSymbol(i, 2-i))));
+        rules.add(new Rule<TicTacToeBoard>(board -> {
+            int countOfFilledCells = 0;
+            for(int i=0; i<3; i++) {
+                for(int j=0; j<3; j++) {
+                    if(board.getSymbol(i, j) != null) {
+                        countOfFilledCells++;
+                    }
+                }
+            }
+            if(countOfFilledCells == 9) {
+                return new GameState(true, "-");
+            }
+
+            return new GameState(false, "-");
+        }));
+
+        return rules;
+    }
+
+
+    private static GameState outerTraversal(BiFunction<Integer, Integer, String> next) {
+        GameState result = new GameState(false, "-");
+        for(int i=0; i<3; i++) {
+            final int ii = i;
+            Function<Integer, String> traversal = (j) -> next.apply(ii, j);
+            GameState innerTraversal = traverse(traversal);
+            if(innerTraversal.isOver()) {
+                result = innerTraversal;
+            }
+        }
+        return result;
+    }
+
+    private static GameState traverse(Function<Integer, String> traversal) {
+        GameState result = new GameState(false, "-");
+        boolean possibleStreak = true;
+        for(int j = 0; j <3; j++) {
+            if(traversal.apply(j) == null || !traversal.apply(0).equals(traversal.apply(j))) {
+                possibleStreak = false;
+                break;
+            }
+        }
+        if(possibleStreak) {
+            result = new GameState(true, traversal.apply(0));
+        }
+        return result;
     }
 
     public void setCell(Cell cell, String symbol) {
