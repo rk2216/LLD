ce6879490184514eb4ec888448dcfe1a52aa711f
rk2216 <rockeshkumawath26@gmail.com>
2026-09-18 06:12:31 +0530

    New functionaliy of detecting fork using bad Wrapper class GameInfo

--- src/main/java/api/RuleEngine.java
+++ src/main/java/api/RuleEngine.java
@@ -1,14 +1,48 @@
 package api;
 
 import boards.TicTacToeBoard;
-import game.Board;
-import game.GameState;
+import game.*;
 
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class RuleEngine {
 
+    public GameInfo getInfo(Board board) {
+        if(board instanceof TicTacToeBoard) {
+            GameState gameState = getState(board);
+            String[] players = new String[]{"X", "O"};
+            for(int index = 0; index < 2; index++) {
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        Board boardCopy = board.copy();
+                        Player player = new Player(players[index]);
+                        boardCopy.move(new Move(new Cell(i, j), player));
+                        boolean canStillWin = false;
+                        for (int k = 0; k < 3; k++) {
+                            for (int l = 0; l < 3; l++) {
+                                Board b = boardCopy.copy();
+                                b.move(new Move(new Cell(k, l), player.flip()));
+                                if (getState(b).getWinner().equals(player.flip().symbol())) {
+                                    canStillWin = true;
+                                    break;
+                                }
+                            }
+                            if (canStillWin) {
+                                break;
+                            }
+                        }
+                        if (canStillWin) {
+                            return new GameInfo(gameState, true, player.flip());
+                        }
+                    }
+                }
+            }
+            return new GameInfo(gameState, false, null);
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
     public GameState getState(Board board) {
         if(board instanceof TicTacToeBoard) {
             TicTacToeBoard board1 = (TicTacToeBoard) board;
@@ -81,3 +115,4 @@ public class RuleEngine {
         return result;
     }
 }
+
