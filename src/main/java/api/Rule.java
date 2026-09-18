2d10038b6cf3cf8fb95a91ea63c9cc67e93650ce
rk2216 <rockeshkumawath26@gmail.com>
2026-09-18 16:01:09 +0530

    RuleSet class. Move logic to TicTacToeBoard. Liskov by not adding getSymbol to Board

--- src/main/java/api/Rule.java
+++ src/main/java/api/Rule.java
@@ -1,4 +1,14 @@
 package api;
 
-public class Rule {
+import game.Board;
+import game.GameState;
+
+import java.util.function.Function;
+
+public class Rule<T extends Board> {
+    Function<T, GameState> condition;
+
+    public Rule(Function<T, GameState> condition) {
+        this.condition = condition;
+    }
 }
--- src/main/java/api/RuleSet.java
+++ src/main/java/api/RuleSet.java
@@ -1,4 +1,29 @@
 package api;
 
-public class RuleSet {
+import game.Board;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+public class RuleSet <T extends Board> implements Iterable<Rule<T>> {
+    Set<Rule<T>> ruleList = new HashSet<>(); //encapsulation example. using Set instead of List
+
+    public void add(Rule<T> boardRule) {
+        ruleList.add(boardRule);
+    }
+
+    @Override
+    public Iterator<Rule<T>> iterator() {
+        return ruleList.iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super Rule<T>> action) {
+        ruleList.forEach(action);
+    }
+
+    @Override
+    public Spliterator<Rule<T>> spliterator() {
+        return ruleList.spliterator();
+    }
 }
