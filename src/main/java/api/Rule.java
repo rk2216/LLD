e3dec6c69edb2edf2dcfda1246d91faaddd79f64
rk2216 <rockeshkumawath26@gmail.com>
2026-09-19 10:28:31 +0530

    Dependency Inversion

--- src/main/java/api/Rule.java
+++ src/main/java/api/Rule.java
@@ -7,6 +7,7 @@ import game.GameState;
 import java.util.function.Function;
 
 public class Rule {
+    // Using TicTacToeBoard instead of CellBoard would violate Dependency Inversion.
     Function<CellBoard, GameState> condition;
 
     public Rule(Function<CellBoard, GameState> condition) {
